06881c7a10b80ed02515a54e5a113dbea3d19a8f
r0ma <superuser@gmail.com>
2022-02-09 16:06:46 +0500

    Убраны пробелы

--- Node.php
+++ Node.php
@@ -16,5 +16,25 @@
 
 class Node implements NodeInterface 
 {
-	// ? ? ?
+    public function __construct(string $name)
+    {
+    	// ...
+    }
+
+    public function __toString(): string
+    {
+        // ...
+    }
+    
+    public function getName(): string
+    {
+        // ...
+    }
+
+    /**
+     * @return Node[]
+     */
+    public function getChildren(): array;
+
+    public function addChild(Node $node): self;
 }
